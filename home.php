f26179eff914688a569a1a065f8c7549c7ce80c9
karljanar <25798556+karljanar@users.noreply.github.com>
2020-09-10 13:26:22 +0300

    poolik kodutöö

--- home.php
+++ home.php
@@ -17,6 +17,7 @@
     $today = new DateTime("now");
     //if($semesterstartdays)
     $semesterdurationdaysfromnow = $today->diff($semesterend);
+    $semesterdurationdaysfromnowdays = $semesterdurationdaysfromnow->format("%r%a")
 ?>
 <!DOCTYPE html><!--browseri jaoks, et kiirelt saaks aru, millega  on tehu-->
 <html lang="et"><!--vajalik nt search enginile, et n'idata mis riigi lehega nt tegu-->
@@ -33,14 +34,15 @@
 </head>
 <body>
     <h1><?php echo $username; ?>i Probleem </h1>
-    <p><font size="5">Leht avati: <?php echo $fulltimenow; ?></font></p>
     <p style="color:green">Särkides ja värkides pole probleemi!</p>
+    <p><font size="5">Leht avati: <?php echo $fulltimenow; ?></font></p>
+    <p><?php echo "Semester kestab ".$semesterdurationdays." päeva.";?></p>
+    <p><?php echo "Semestri lõpuni on ".$semesterdurationdaysfromnowdays." päeva.";?></p>
     <p style="color:springgreen">Leht loodud veebiproge kursuse raames <a href="https://www.tlu.ee/dt" style="color: snow">TLU Digitehnoloogiate Instituudis.</a></p>
     <img src="les.jpg">
     <p style="color:firebrick"><font size="5">Siin on kajastatud esimene pilt, mis laeti internetti <br>ning lehe taustaks on täiesti lambi värv.</font></p>
     <p style="color:greenyellow">Aega läks, aga lõpuks sai serverile ligi.</p>
     <p><?php echo "Parajasti on ".$partofday."."; ?></p>
-    <p><?php echo "Semestri lõpuni on ".$semesterdurationdays." päeva.";?></p>
     <p style="color:springgreen">Koodi asjad leitavad <a href="https://github.com/karljanar/vp" style="color: snow">GitHubis.</a></p>
 </body>
 </html>
