7220c46fc0d53cf11e17f495ab78fe27d5822cc1
karljanar <25798556+karljanar@users.noreply.github.com>
2020-09-18 16:19:24 +0300

    kodutöö

--- header.php
+++ header.php
@@ -1,14 +1,41 @@
-<!DOCTYPE html><!--browseri jaoks, et kiirelt saaks aru, millega  on tehu-->
-<html lang="et"><!--vajalik nt search enginile, et n'idata mis riigi lehega nt tegu-->
+<?php
+$username = "Janar";
+require("../../config.php");
+?>
+<!DOCTYPE html>
+<html lang="et">
 <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $username; ?> ehitab lehte</title>
     <style>
         body {
+
             background-color: #2e3440;
             background-repeat: no-repeat;
             background-size: auto;
-            color:whitesmoke;
+            color: whitesmoke;
+            font-family: Arial, Helvetica, sans-serif;
         }
-    </style>
-</head>
+        /* Add a black background color to the top navigation */
+        .topnav {
+            background-color: #2e3440;
+            overflow: hidden;
+        }
+        /* Style the links inside the navigation bar */
+        .topnav a {
+            float: none;
+            color: whitesmoke;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+        .topnav a.active {
+            background-color: firebrick;
+            color: white;
+        }
+
