fa01b034c122c5b4a671b98b58f563edf5c14260
karljanar <25798556+karljanar@users.noreply.github.com>
2020-09-26 23:59:58 +0300

    mp3 video

--- home.php
+++ home.php
@@ -112,5 +112,6 @@ $imghtml ='<img src="vp_pics/'. $picfiles[$picnum] .'" alt="pildid TLUst" class=
     <hr>
     <?php echo $imghtml; ?>
     <hr>
+     <div style="width:100%;height:0px;position:relative;padding-bottom:75.000%;"><iframe src="https://streamable.com/e/4jljlx?loop=0" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
 </body>
 </html>
