bcaea234ff6b20b7f59cbdd6a946c41a59b875f1
karljanar <25798556+karljanar@users.noreply.github.com>
2020-10-28 15:00:47 +0200

    git ignore

--- photoupload.php
+++ photoupload.php
@@ -3,18 +3,118 @@
     require("header.php");
     //kas vajutati salvestus nuppu
     $inputerror = "";
+    $fileuploadsizelimit = 1048576;
+    $notice = "";
+    $filename = "";
+    $filenameprefix = "vp_";
+    $fileuploaddir_orig = "photoupload_orig/";
+    $photomaxw = 600;
+    $photomaxh = 400;
+    $fileuploaddir_normal = "photoupload_normal/";
+
 
     if(isset($_POST["photosubmit"])){
        //var_dump($_POST);
        //var_dump($_FILES);
        //kas on pilt
        $check = getimagesize($_FILES["photoinput"]["tmp_name"]);
-       if($check !== false){
-           
-       }
+        if($check !== false){
+            if($check["mime"] == "image/jpeg"){
+                $filetype = "jpg";
+            }
+            if($check["mime"] == "image/png"){
+                $filetype = "png";
+            }
+            if($check["mime"] == "image/gif"){
+                $filetype = "gif";
+            } 
+        } else {
+            $inputerror = "Valitud fail ei ole pilt. ";
+        }
+
+       //ega pole liiga suur fail
+        if($_FILES["photoinput"]["size"] > $fileuploadsizelimit){
+           $inputerror .= "Fail on liiga suur. ";
+        }
+
+        //genereerime faili nime
+
+        $timestamp = microtime(1) * 10000;
+        $filename = $filenameprefix .$timestamp ."." .$filetype;
 
 
-       move_uploaded_file($_FILES["photoinput"]["tmp_name"], "photoupload_orig/" .$_FILES["photoinput"]["name"]);
+       //kas fail olemas
+        if(file_exists($fileuploaddir_orig .$filename)){
+            $inputerror .= "See fail juba eksisteerib. ";
+        }
+
+        if(empty($inputerror)){
+            //teen pildi vaiksemaks
+            //loome iamge objekti ehk pikslikogu
+            if($filetype == "jpg"){
+                $mytempimage = imagecreatefromjpeg($_FILES["photoinput"]["tmp_name"]);
+            }
+            if($filetype == "png"){
+                $mytempimage = imagecreatefrompng($_FILES["photoinput"]["tmp_name"]);
+            }
+            if($filetype == "gif"){
+                $mytempimage = imagecreatefromgif($_FILES["photoinput"]["tmp_name"]);
+            }
+            //pildi orig suurus
+            $imagew = imagesx($mytempimage);
+            $imageh = imagesy($mytempimage);
+            //kas laius v korgus ja leian vahendamise kordaja
+            if($imagew / $photomaxw > $imageh / $photomaxh){
+                $photosizeratio = $imagew / $photomaxw;
+            } else {
+                $photosizeratio = $imageh / $photomaxh;
+            }
+            //arvutan uued piirid
+            $neww = round($imagew / $photosizeratio);
+            $newh = round($imageh / $photosizeratio);
+            //loon uue suurusega pildi objekti
+            $mynewtempimage = imagecreatetruecolor($neww, $newh);
+            //sailitamaks png piltide labipaistvust
+            imagesavealpha($mynewtempimage, true);
+            $transparentcolor = imagecolorallocatealpha($mynewtempimage, 0, 0, 0, 127);
+            imagefill($mynewtempimage, 0, 0, $transparentcolor);
+
+            imagecopyresampled($mynewtempimage, $mytempimage, 0, 0, 0, 0, $neww, $newh, $imagew, $imageh);
+            
+            //vahendatud pilt faili
+            if($filetype == "jpg"){
+                if(imagejpeg($mynewtempimage, $fileuploaddir_normal .$filename, 90)){
+                    $notice .= "Vahendatud pildi salvestamine timmis. ";
+                } else {
+                    $notice .= "Vahendatud pildi salvestamine ebaonnestus. ";
+                }
+            }
+            if($filetype == "png"){
+                if(imagepng($mynewtempimage, $fileuploaddir_normal .$filename, 6)){
+                    $notice .= "Vahendatud pildi salvestamine timmis. ";
+                } else {
+                    $notice .= "Vahendatud pildi salvestamine ebaonnestus. ";
+                }
+            }
+            if($filetype == "gif"){
+                if(imagegif($mynewtempimage, $fileuploaddir_normal .$filename)){
+                    $notice .= "Vahendatud pildi salvestamine timmis. ";
+                } else {
+                    $notice .= "Vahendatud pildi salvestamine ebaonnestus. ";
+                }
+            }
+            imagedestroy($mynewtempimage);
+            imagedestroy($mytempimage);
+
+            if(move_uploaded_file($_FILES["photoinput"]["tmp_name"], $fileuploaddir_orig .$filename)){
+                $notice .= "Timmis!";
+            } else {
+                $notice .= "Tekkis viga!";
+            }
+            
+
+        }
+       
     }
 ?>
         .pilt {
@@ -73,7 +173,9 @@
         <br>
         <input type="submit" name="photosubmit" value="Lae pilt üles">
     </form>
-    <p><?php echo $inputerror; ?></p>
+    <?php echo $inputerror;
+    echo $notice;
+    ?>
     </body>
 </html>
 
